3df2786a6012516f08309f9b7cc98839d19c83cb
Shivam Mevawala <shivmevawala@gmail.com>
2013-10-22 13:58:43 -0400

    worked on ps4

--- catgrepmore.c
+++ catgrepmore.c
@@ -5,13 +5,55 @@
 #include <fcntl.h> 
 #include <string.h> 
 #include <errno.h> 
-
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 extern int errno;
     
 
+int execgrepmore(){
+    pid_t pidgrep,cpidgrep, pidmore, cpidmore;
+    unsigned status;
+    switch(pidgrep = fork()) {
+    	case -1:
+    		perror("Fork failed");
+    		exit(1);
+    		break;
+    	case 0:
+    		//in grep child
+    		
+    		break;
+    	default:
+			while ((cpidgrep=wait(&status)) != pidgrep){
+				if (cpidgrep<0){
+					perror("Wait failed");
+					break;
+				}
+			};
+			switch(pidmore = fork()) {
+		    	case -1:
+		    		perror("Fork failed");
+		    		exit(1);
+		    		break;
+		    	case 0:
+		    		//in more child
+		    		break;
+		    	default:
+					while ((cpidmore=wait(&status)) != pidmore){
+						if (cpidmore<0){
+							perror("Wait failed");
+							break;
+						}
+					};
+			}
+    		break;
+	}
+return 0;
 
+}
     
-int main (int argc, char **argv){
+int main (int argc, char **argv) {
 
 
 
