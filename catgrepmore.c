f0f4c38eba178c261449acd8f228580cfe309753
Shivam Mevawala <shivmevawala@gmail.com>
2013-10-23 17:00:49 -0400

    catgrepmore works except for large files

--- catgrepmore.c
+++ catgrepmore.c
@@ -11,40 +11,73 @@
 #include <sys/resource.h>
 extern int errno;
 
-int files_read;
+int files_read, bytes_written;
 
 void sighand(int sig){
-	fprintf(stderr, "Amount of files read:%i\tAmount of bytes read in current file:%i\n",files_read-2,5); 
+	fprintf(stderr, "Amount of files read:%i\tAmount of bytes read:%i\n",files_read-1,bytes_written); 
 	exit(1);
 }
 
-int fileread(char * filename, int fp){
+int fileread(char * filepath, int fds){
+	int infp, wb, iread, buff=1024, over, sofar=0;
+	char rb[1024];
+	if ((infp=open(filepath,O_RDONLY))<0){
+		perror("File opening error");
+		return 1;
+	}
+	over=16*1024*1024;
+	over = over-1024;
+	printf("%i\n",over);
+	while((iread=read(infp,rb,buff))>0 && sofar<over){
+		wb=write(fds,rb,iread);
+		if(wb<0){
+			perror("Write failure");
+		}
+		else if(wb<iread){
+			perror("Partial write");
+		}
+		sofar=sofar+wb;
+		printf("%i\n",sofar);
+		bytes_written=bytes_written+wb;
+	}
+
+	if(close(infp)<0){
+		perror("File closing error");
+	}
 	return 0;
 }
+
 int execgrepmore(char *filepath, char *pattern){
     pid_t pidgrep,cpidgrep, pidmore, cpidmore;
   	char grepstr[8];
     unsigned status;
-    int fdsi[2], fdsj[2], ofds;
-	char *commandg[3], *commandm[2];
+    int fdsi[2], fdsfile[2], ofds;
+   	char *commandg[3], *commandm[2];
 
     commandg[0]="grep";
     commandg[1]=pattern;
     commandg[2]=NULL;
     commandm[0]="more";
     commandm[1]=NULL;
-    if((ofds= open(filepath,O_RDONLY))<0){
-    	perror("Error opening file");
+
+
+    if(pipe(fdsfile)<0){
+    	perror("Cannot create pipe");
     	return 1;
     }
     if(pipe(fdsi)<0){
     	perror("Cannot create pipe");
     	return 1;
     }
-    // printf("%s\n",commandg[0]);
+	printf("file to  be read\n");
+	if((fileread(filepath,fdsfile[1]))==1)
+		return 1;
+	printf("file read\n");	
+	if(close(fdsfile[1])<0)
+		perror("Pipe closing  error");
 
 
-    switch(pidgrep = fork()) {
+   	switch(pidgrep = fork()) {
     	case -1:
     		perror("Fork failed");
     		exit(1);
@@ -52,20 +85,18 @@ int execgrepmore(char *filepath, char *pattern){
     	case 0:
     		//in grep child
     		printf("grepping %s for pattern %s\n", filepath, commandg[1]);
-		    if((ofds= open(filepath,O_RDONLY))<0){
-    			perror("Error opening file");
-    			return 1;
-    		}
-    		dup2(ofds,0);
+
+    		dup2(fdsfile[0],0);
     		dup2(fdsi[1],1);
     		if(execvp(commandg[0],commandg)<0){
     			perror("Execution error");
     			return 1;
     		}
-    		if(close(ofds)<0)
-    			perror("File closing error");
+			if(close(fdsfile[0])<0)
+				perror("Pipe closing  error");	
 			if(close(fdsi[1])<0)
 				perror("Pipe closing  error");
+		
     		break;
 
     	default:
@@ -75,6 +106,9 @@ int execgrepmore(char *filepath, char *pattern){
 					break;
 				}
 			};
+
+			if(close(fdsfile[0])<0)
+				perror("Pipe closing  error");	
 			if(close(fdsi[1])<0)
 				perror("Pipe closing  error");
 			switch(pidmore = fork()) {
@@ -107,14 +141,13 @@ int execgrepmore(char *filepath, char *pattern){
 	}
 return 0;
 }
-    
+   
 int main (int argc, char **argv) {
 
 	signal(SIGINT, sighand);
 	for(files_read=2;files_read<argc;files_read++){
 		execgrepmore(argv[files_read],argv[1]);
+		// printf("%i\n",files_read);
 	}
 	return 0;
 }
-
-
