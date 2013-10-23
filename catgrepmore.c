014b53bac4c62eb04e62e0c5fcba3e0aa456d355
Shivam Mevawala <shivmevawala@gmail.com>
2013-10-23 12:13:13 -0400

    catgrepmore works without signal interuption

--- catgrepmore.c
+++ catgrepmore.c
@@ -17,13 +17,22 @@ int execgrepmore(char *filepath, char *pattern){
   	char grepstr[8];
     unsigned status;
     int fdsi[2], fdsj[2], ofds;
-	char *command[3];
+	char *commandg[3], *commandm[2];
 
-    command[0]="grep";
-    command[1]=pattern;
-    command[2]=NULL;
-    ofds= open(filepath,O_RDONLY);
-    // printf("%s\n",command[0]);
+    commandg[0]="grep";
+    commandg[1]=pattern;
+    commandg[2]=NULL;
+    commandm[0]="more";
+    commandm[1]=NULL;
+    if((ofds= open(filepath,O_RDONLY))<0){
+    	perror("Error opening file");
+    	return 1;
+    }
+    if(pipe(fdsi)<0){
+    	perror("Cannot create pipe");
+    	return 1;
+    }
+    // printf("%s\n",commandg[0]);
 
 
     switch(pidgrep = fork()) {
@@ -33,12 +42,23 @@ int execgrepmore(char *filepath, char *pattern){
     		break;
     	case 0:
     		//in grep child
-    		printf("grepping %s for pattern %s\n", filepath, command[1]);
-    		dup2(ofds,fileno(stdin));
-    		if(execvp(command[0],command)<0)
-    			perror("execution error");
-    		
+    		printf("grepping %s for pattern %s\n", filepath, commandg[1]);
+		    if((ofds= open(filepath,O_RDONLY))<0){
+    			perror("Error opening file");
+    			return 1;
+    		}
+    		dup2(ofds,0);
+    		dup2(fdsi[1],1);
+    		if(execvp(commandg[0],commandg)<0){
+    			perror("Execution error");
+    			return 1;
+    		}
+    		if(close(ofds)<0)
+    			perror("File closing error");
+			if(close(fdsi[1])<0)
+				perror("Pipe closing  error");
     		break;
+
     	default:
 			while ((cpidgrep=wait(&status)) != pidgrep){
 				if (cpidgrep<0){
@@ -46,6 +66,8 @@ int execgrepmore(char *filepath, char *pattern){
 					break;
 				}
 			};
+			if(close(fdsi[1])<0)
+				perror("Pipe closing  error");
 			switch(pidmore = fork()) {
 		    	case -1:
 		    		perror("Fork failed");
@@ -53,6 +75,14 @@ int execgrepmore(char *filepath, char *pattern){
 		    		break;
 		    	case 0:
 		    		//in more child
+		    		dup2(fdsi[0],0);
+		    		if(execvp(commandm[0],commandm)<0){
+    					perror("Execution error");
+    					return 1;
+    				}
+    				if(close(fdsi[0]<0)){
+    					perror("Pipe closing error");
+    				}
 		    		break;
 		    	default:
 					while ((cpidmore=wait(&status)) != pidmore){
@@ -60,12 +90,13 @@ int execgrepmore(char *filepath, char *pattern){
 							perror("Wait failed");
 							break;
 						}
-					};
+					}
+		    		if(close(fdsi[0])<0)
+    					perror("Pipe closing error");
 			}
     		break;
 	}
 return 0;
-
 }
     
 int main (int argc, char **argv) {
@@ -74,8 +105,7 @@ int main (int argc, char **argv) {
 	for(i=2;i<argc;i++){
 		execgrepmore(argv[i],argv[1]);
 	}
-
-
-
 	return 0;
 }
+
+
