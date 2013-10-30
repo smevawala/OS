8998ca08945b7316e75d477813c4a894c86ccfc7
Shivam Mevawala <shivmevawala@gmail.com>
2013-10-30 18:09:05 -0400

    fixed catgrepmore

--- catgrepmore.c
+++ catgrepmore.c
@@ -17,16 +17,21 @@ void sighand(int sig){
 	fprintf(stderr, "Amount of files read:%i\tAmount of bytes read:%i\n",files_read-1,bytes_written); 
 	exit(1);
 }
+void sigpipehand(int sig){
+	fprintf(stderr, "Sigpipe occured\n"); 
+	exit(1);
+}
 
 int fileread(char * filepath, int fds){
-	int infp, wb, iread, buff=1024,sofar=0;
+	int infp, wb, iread, buff=1024;
 	char *rb[1024];
 
 	if ((infp=open(filepath,O_RDONLY))<0){
 		perror("File opening error");
 		return 1;
 	}
-	while((iread=read(infp,rb,buff))>0 && sofar<65536){
+	//&& sofar<65536
+	while((iread=read(infp,rb,buff))>0 ){
 		wb=write(fds,rb,iread);
 		if(wb<0){
 			perror("Write failure");
@@ -35,7 +40,7 @@ int fileread(char * filepath, int fds){
 			perror("Partial write");
 		}
 		bytes_written=bytes_written+wb;
-		sofar=sofar+wb;
+		// sofar=sofar+wb;
 	}
 	if(close(infp)<0){
 		perror("File closing error");
@@ -45,9 +50,9 @@ int fileread(char * filepath, int fds){
 
 int execgrepmore(char *filepath, char *pattern){
     pid_t pidgrep,cpidgrep, pidmore, cpidmore;
-  	char grepstr[8];
+  	// char grepstr[8];
     unsigned status;
-    int fdsi[2], fdsfile[2], ofds;
+    int fdsi[2], fdsfile[2];
    	char *commandg[3], *commandm[2];
 
     commandg[0]="grep";
@@ -64,10 +69,7 @@ int execgrepmore(char *filepath, char *pattern){
     	perror("Cannot create pipe");
     	return 1;
     }
-	if((fileread(filepath,fdsfile[1]))==1)
-		return 1;	
-	if(close(fdsfile[1])<0)
-		perror("Pipe closing  error");
+
 
 
    	switch(pidgrep = fork()) {
@@ -77,35 +79,27 @@ int execgrepmore(char *filepath, char *pattern){
     		break;
     	case 0:
     		//in grep child
-    		printf("grepping %s for pattern %s\n", filepath, commandg[1]);
+			if(close(fdsfile[1])<0)
+				perror("Pipe closing  error");
+			if(close(fdsi[0])<0)
+				perror("Pipe closing  error");	
 
+    		printf("grepping %s for pattern %s\n", filepath, commandg[1]);
     		dup2(fdsfile[0],0);
     		dup2(fdsi[1],1);
+    		// fprintf(stderr,"starting with grep\n");
     		if(execvp(commandg[0],commandg)<0){
     			perror("Execution error");
     			return 1;
     		}
+    		// fprintf(stderr,"done with grep\n");
 			if(close(fdsfile[0])<0)
 				perror("Pipe closing  error");	
 			if(close(fdsi[1])<0)
 				perror("Pipe closing  error");
-			if(close(fdsi[0])<0)
-				perror("Pipe closing  error");			
-		
     		break;
 
     	default:
-			while ((cpidgrep=wait(&status)) != pidgrep){
-				if (cpidgrep<0){
-					perror("Wait failed");
-					break;
-				}
-			};
-
-			if(close(fdsfile[0])<0)
-				perror("Pipe closing  error");	
-			if(close(fdsi[1])<0)
-				perror("Pipe closing  error");
 			switch(pidmore = fork()) {
 		    	case -1:
 		    		perror("Fork failed");
@@ -113,25 +107,53 @@ int execgrepmore(char *filepath, char *pattern){
 		    		break;
 		    	case 0:
 		    		//in more child
+					if(close(fdsfile[1])<0)
+						perror("Pipe closing  error");
+					if(close(fdsfile[0])<0)
+						perror("Pipe closing  error");	
+					if(close(fdsi[1])<0)
+						perror("Pipe closing  error");
+
 		    		dup2(fdsi[0],0);
+		    		// fprintf(stderr,"starting with more\n");	
 		    		if(execvp(commandm[0],commandm)<0){
     					perror("Execution error");
     					return 1;
     				}
-    				if(close(fdsi[0]<0)){
+		    		// fprintf(stderr,"done with more\n");	
+    				if(close(fdsi[0]<0))
     					perror("Pipe closing error");
-    				}
+
 		    		break;
 		    	default:
+					if(close(fdsfile[0])<0)
+						perror("Pipe closing  error");	
+					if(close(fdsi[1])<0)
+						perror("Pipe closing  error");
+		    		if(close(fdsi[0])<0)
+    					perror("Pipe closing error");
+					if((fileread(filepath,fdsfile[1]))==1)
+						return 1;	
+					if(close(fdsfile[1])<0)
+						perror("Pipe closing  error");
+					while ((cpidgrep=wait(&status)) != pidgrep){
+						if (cpidgrep<0){
+							perror("Wait failed");
+							break;
+						}
+					}
+					// printf("done with grep process\n");					
 					while ((cpidmore=wait(&status)) != pidmore){
 						if (cpidmore<0){
 							perror("Wait failed");
 							break;
 						}
 					}
-		    		if(close(fdsi[0])<0)
-    					perror("Pipe closing error");
+					// printf("done with more process\n");
 			}
+
+
+			
     		break;
 	}
 return 0;
@@ -140,6 +162,8 @@ return 0;
 int main (int argc, char **argv) {
 
 	signal(SIGINT, sighand);
+	signal(SIGPIPE, sigpipehand);
+
 	for(files_read=2;files_read<argc;files_read++){
 		execgrepmore(argv[files_read],argv[1]);
 		// printf("%i\n",files_read);
