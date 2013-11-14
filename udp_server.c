14e1f839292cb800dca11cab706f78af26d8a0cb
Shivam Mevawala <shivmevawala@gmail.com>
2013-11-14 01:13:28 -0500

    done i guess

---
+++ udp_server.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/sysinfo.h>
+
+int main(int argc, char ** argv){
+	int port, sd, fd, len;
+	char buf[INET6_ADDRSTRLEN];
+	struct sysinfo info;
+	sysinfo(&info);
+	port=atoi(argv[1]);
+	if((sd=socket(AF_INET, SOCK_DGRAM, 0))<0){
+		perror("socket error");
+		return -1;
+	}
+	struct sockaddr_in sockaddr;
+	struct sockaddr_storage sender;
+	sockaddr.sin_family=AF_INET;
+	sockaddr.sin_addr.s_addr=INADDR_ANY;
+	sockaddr.sin_port=htons(port);
+	len=sizeof sockaddr;
+
+	if(bind(sd,(struct sockaddr *)&sockaddr,len)<0){
+		perror("bind error");
+		return -1;
+	}
+
+	char * hostname;
+	// inet_ntop(sender.ss_family, &(((struct sockaddr_in *)&sender)->sin_addr), buf, sizeof buf );
+	// struct hostent *addr;
+	// addr=gethostbyaddr(buf, sizeof buf, AF_INET);
+	// if(addr==NULL){
+	// 	// herror(" ");
+	// 	// return -1;
+	// } 
+	// else{
+	// 	hostname=addr->h_name;
+	// }
+	int rc,wc;
+	char rb[1024], t[512], u[512];
+	time_t start;
+	while(1){
+		memset(rb,0,sizeof rb);
+		memset(u,0,sizeof u);
+		memset(t,0,sizeof t);
+		rc=recvfrom(sd, rb, 1024, 0, (struct sockaddr*) &sender, &len);
+		if(rc<0)
+			perror("receive error");
+		printf("%s\n",rb);
+		if(!strcmp(rb, "DATE")){
+			ctime_r(&start,t);
+			printf("Sending date: %s\n",t);
+			sendto(sd,t,sizeof t, 0, (struct sockaddr*) &sender, len);
+		}
+		if(!strcmp(rb, "UPTIME")){
+			sprintf(u, "%ld", info.uptime);
+			printf("Sending Uptime: %s\n",t);
+			sendto(sd,u,sizeof u, 0, (struct sockaddr*) &sender, len);
+		}	
+	}
+	return 0;
+}
